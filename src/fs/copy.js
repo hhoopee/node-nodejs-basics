2127d3fda1b5e869e8ffcc5f558facd65ca53dc3
hhoopee <vakylskijdev@gmail.com>
2024-01-23 21:04:35 +0300

    feat: add solution to fs-copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,47 @@
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const originalFolderPath = path.join(__dirname, "files");
+const copiedFolderPath = path.join(__dirname, "files_copy");
+
+const checkCopiedFolder = async () => {
+  try {
+    await fs.access(copiedFolderPath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+  }
+};
+
+const checkOriginalFolder = async () => {
+  try {
+    await fs.access(originalFolderPath);
+  } catch (err) {
+    throw err;
+  }
+};
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(originalFolderPath);
+    await checkOriginalFolder();
+    await checkCopiedFolder();
+    await fs.mkdir(copiedFolderPath);
+    files.forEach(async (file) => {
+      await fs.copyFile(
+        path.join(originalFolderPath, file),
+        path.join(copiedFolderPath, file)
+      );
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 await copy();
