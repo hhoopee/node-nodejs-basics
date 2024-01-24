e6ef6dce38ecabe509379657db1ead6e47729491
hhoopee <vakylskijdev@gmail.com>
2024-01-24 07:43:24 +0300

    feat: add solution to fs-rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,46 @@
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const originalFile = path.join(__dirname, "files", "wrongFilename.txt");
+const copiedFile = path.join(__dirname, "files", "properFilename.md");
+
+const checkOriginalFile = async () => {
+  try {
+    await fs.access(originalFile);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
+};
+
+const checkCopiedFile = async () => {
+  try {
+    await fs.access(copiedFile);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+  }
+};
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await checkOriginalFile();
+    await checkCopiedFile();
+    await fs.rename(originalFile, copiedFile);
+    console.log(
+      'Successfully renamed "wrongFilename.txt" to "properFilename.md"'
+    );
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 await rename();
