a9886c5f3374474c351f4b798a28881a641fd824
hhoopee <vakylskijdev@gmail.com>
2024-01-25 22:00:34 +0300

    feat: add solution to cli-args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const filteredArgs = args.filter((arg) => arg.startsWith("--"));
+  filteredArgs.forEach((arg) => {
+    console.log(`${arg.slice(2)} is ${args[args.indexOf(arg) + 1]}`);
+  });
 };
 
 parseArgs();
