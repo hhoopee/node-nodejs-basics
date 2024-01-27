e4033e62522b20cc028ef23f5b9c77324f927388
hhoopee <vakylskijdev@gmail.com>
2024-01-27 20:38:39 +0300

    feat: add solution to strams-write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileTowrite.txt");
+  const writeStream = fs.createWriteStream(filePath);
+  process.stdin.pipe(writeStream);
+
+  process.stdin.on("data", (chunk) => {
+    const chunkString = chunk.toString();
+    if (chunkString.match("STOP")) process.stdin.unpipe(writeStream);
+  });
 };
 
 await write();
