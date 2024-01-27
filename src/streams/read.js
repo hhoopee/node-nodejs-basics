4d149b60729fe4b77f6d2180ed3f3ce56ae3651c
hhoopee <vakylskijdev@gmail.com>
2024-01-27 18:32:35 +0300

    feat: add solution to strams-read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+  const readStream = fs.createReadStream(filePath);
+  readStream.pipe(process.stdout);
+  // readStream.on('data', (chunk) => {
+  //     process.stdout.write(chunk)
+  // })
 };
 
 await read();
