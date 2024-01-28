cbcfe8883ba0784cec5aa7d939a6cc76de38a1b7
hhoopee <vakylskijdev@gmail.com>
2024-01-28 07:39:35 +0300

    feat: add solution to strams-transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const transformC = new Transform({
+    transform(chunk, enc, callback) {
+      const chunkString = chunk.toString().trim();
+      const reverseChunkString = chunkString.split("").reverse("").join("");
+      this.push(reverseChunkString + "\n");
+      callback();
+    },
+  });
+  process.stdin.pipe(transformC).pipe(process.stdin);
+
+  process.stdin.on("data", (chunk) => {
+    const chunkString = chunk.toString();
+    if (chunkString.match("STOP")) process.stdin.unpipe(transformC);
+  });
 };
 
 await transform();
